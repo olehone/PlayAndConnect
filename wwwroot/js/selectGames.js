cb889255bfd9030689dbd13a3fd46ec454632e40
olehvolodko <oleh.volodko.ri.2022@lpnu.ua>
2023-05-24 14:36:47 +0300

    Correct settings

--- wwwroot/js/selectGames.js
+++ wwwroot/js/selectGames.js
@@ -1,6 +1,6 @@
 function getGameOptions() {
-    var gameName = $('#gameNameInput').val();
-    
+  var gameName = $('#gameNameInput').val().trim();
+  if (gameName.length > 0&& gameName.length<30) {
     $.ajax({
       url: '/Home/GetGameOptions',
       type: 'POST',
@@ -13,16 +13,24 @@ function getGameOptions() {
       }
     });
   }
-  
-  function populateGameOptions(options) {
-    var gameOptionsSelect = $('#gameOptions');
-    gameOptionsSelect.empty();
-  
-    // Додавання варіантів у випадаючий список
-    for (var i = 0; i < options.length; i++) {
-      var option = options[i];
-      var optionElement = $('<option>').text(option);
-      gameOptionsSelect.append(optionElement);
-    }
+}
+
+function populateGameOptions(options) {
+  console.log(options);
+  var gameOptionsSelect = $('#gameOptions');
+  gameOptionsSelect.empty();
+
+  // Додавання варіантів у випадаючий список
+  for (var i = 0; i < options.length; i++) {
+    var option = options[i];
+    var optionElement = $('<option>').text(option.title).val(option.id);
+    gameOptionsSelect.append(optionElement);
   }
-  
+
+  // При виборі гри автоматично записуємо її назву в рядок пошуку
+  gameOptionsSelect.on('change', function() {
+    var selectedGame = $(this).find('option:selected').text();
+    $('#gameNameInput').val(selectedGame);
+  });
+}
+
